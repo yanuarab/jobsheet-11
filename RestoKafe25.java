b8f9284602fb775fd540a706ab9fcfb66e875914
tugas <aldayanuar6@gmail.com>
2024-11-21 20:35:45 +0700

    percobaan 2

---
+++ RestoKafe25.java
@@ -0,0 +1,24 @@
+public class RestoKafe25 {
+
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+        
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("===========================");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+     public static void main(String[] args) {
+        Menu("Andi", true);
+    }
+}
